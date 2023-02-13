2eb6f68808fcbd7047f11ebc859e74dbaa6106bb
Eugene Kuchaev <eugenekuchaev@gmail.com>
2023-02-13 22:05:59 +0100

    Added client-side authentication

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {}
+
+  constructor(public accountService: AccountService) {}
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => {
+        console.log(error);
+      }
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+}
